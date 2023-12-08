5548df472ef29fd18df7f6ec2bf527cf1f312e60
Dina Nur Auliana <dinakayla29@gmail.com>
2023-12-08 10:33:52 +0700

    first commit

---
+++ app/Http/Controllers/Api/V1/TaskController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Task\StoreTaskRequest;
+use App\Http\Requests\Task\UpdateTaskRequest;
+use App\Http\Resources\TaskResource;
+use App\Models\Task;
+use Exception;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $tasks = TaskResource::collection(Task::all());
+
+        return response()->json([
+            'message' => 'success',
+            'data' => $tasks
+        ], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTaskRequest $request)
+    {
+        $tasks = Task::create($request->validated());
+
+        $task =  TaskResource::make($tasks);
+        return response()->json([
+            'message' => 'success',
+            'data' => $task
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Task $task)
+    {
+        $task = TaskResource::make($task);
+
+        try {
+            if ($task != null) {
+                return response()->json([
+                    'message' => 'success',
+                    'data' => $task
+                ], 200);
+            } else {
+                return response()->json([
+                    'message' => 'error',
+                ], 400);
+            }
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'server error',
+            ], 500);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTaskRequest $request, Task $task)
+    {
+        $task->update($request->validated());
+
+        return TaskResource::make($task);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Task $task)
+    {
+        $task->delete();
+
+        return response()->json([
+            'message' => 'succes is deleted data'
+        ]);
+    }
+}
---
+++ app/Http/Requests/Task/UpdateTaskRequest.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Http\Requests\Task;
+
+
+class UpdateTaskRequest extends StoreTaskRequest
+{
+}
